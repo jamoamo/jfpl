abf551750e15dba34dd027fb09d6063215423c3d
James Amoore <jamoamo@users.noreply.github.com>
2021-08-28 21:48:46 +0200

    Remove hard-coded credentials

--- src/test/java/com/github/jamoamo/jfpl/TestApp.java
+++ src/test/java/com/github/jamoamo/jfpl/TestApp.java
@@ -18,10 +18,8 @@ public class TestApp
 	{
 		FPL fpl = new FPL();
 		FPLLoginCredentials creds = new FPLLoginCredentials();
-		//creds.setUsername(args[0]);
-		creds.setUsername("amoore.james@gmail.com");
-		//creds.setPassword(args[1]);
-		creds.setPassword("r5w2TVVQbblj");
+		creds.setUsername(args[0]);
+		creds.setPassword(args[1]);
 		fpl.login(creds);
 		FPLUser user = fpl.getCurrentUser();
 		System.out.println(user.getTeamName());
